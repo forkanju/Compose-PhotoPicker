cab40530559c73f1a43e699ee95c141914f37866
Forkan <automation19@mis.prangroup.com>
2023-10-21 12:04:45 +0600

    Initial commit

---
+++ app/src/main/java/com/example/compose_photopicker/MainScreen.kt
@@ -0,0 +1,86 @@
+package com.example.compose_photopicker
+
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+@Composable
+fun MainScreen() {
+    val context = LocalContext.current
+    var imageUri: Any? by remember {
+        mutableStateOf(R.drawable.picker)
+    }
+    val photoPicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia()
+    ) {
+        if (it != null) {
+            Log.d("PhotoPicker", "Selected URI: $it")
+            imageUri = it
+        } else {
+            Log.d("PhotoPicker", "No media selected")
+        }
+    }
+    //    val multiplePhotoPicker = rememberLauncherForActivityResult(
+//        contract = ActivityResultContracts.PickMultipleVisualMedia(maxItems = 2)
+//    ) {
+//
+//    }
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .size(250.dp)
+                .clickable {
+                    photoPicker.launch(
+                        PickVisualMediaRequest(
+                            ActivityResultContracts.PickVisualMedia.ImageOnly
+                        )
+                    )
+                },
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(imageUri)
+                .crossfade(enable = true)
+                .build(),
+            contentDescription = "Avatar Image",
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = {
+            Toast.makeText(
+                context,
+                ActivityResultContracts.PickVisualMedia.isPhotoPickerAvailable().toString(),
+                Toast.LENGTH_SHORT
+            ).show()
+        }) {
+            Text(text = "Availability")
+        }
+    }
+}
